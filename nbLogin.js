71ffea244849d3a7e8e92c6dd579e8ff3b5cb8b1
Nathan Berwanger <90463884+JetMardium@users.noreply.github.com>
2021-09-26 13:18:56 -0400

    Update nbLogin.js

--- nbLogin.js
+++ nbLogin.js
@@ -4,14 +4,20 @@ function checkCreds() {
     var Name2 = document.getElementById("lastName").value;
     var badgeNum = document.getElementById("numBadge").value;
     var loginInfo = Name1 + " " + Name2;
-    //This condition tells it to check if the login name is of a proper length and run if the condition is true.
+
+    //This condition tells it to check if the login name is of a proper length and run if the condition is true. I split the process into two so that the first and last name both must have a set limit of characters rather than a combined total.
     //More advanced coders than I can make this process specific to individual user names, or even user names in a database that it runs a check against.
-    if (loginInfo.length >20 || loginInfo.length <4) {
-          document.getElementById("loginStatus").innerHTML = "Login ID has invalid number of characters!";
+    if (Name1.length >20 || Name1.length <4) {
+          document.getElementById("loginStatus").innerHTML = "User First Name has invalid number of characters!";
           
-          //This condition tells it to check for this condition if the first one is false, and run if this number is true
+    }
+        else
+        if (Name2.length >20 || Name2.length <4) {
+            document.getElementById("loginStatus").innerHTML = "User Last Name has invalid number of characters!";
+    }
+
+              //This condition tells it to check for this condition if the first one is false and the second one is as well, and run if this number is true
           //In this case I made it run if the proper numbers are input
-    } 
         else
         if (badgeNum < 999  && badgeNum > 100) {
             alert("Training Progarm Accesss Granted. Welcome to the UAT Deep Space Exploration Team " + loginInfo + ".");
@@ -19,7 +25,7 @@ function checkCreds() {
 
     }
 
-    //This condition runs if both the first and second conditions are false, and it tells the system to display that the badge number is invalid.
+    //This condition runs if all three of the other conditions are false, and it tells the system to display that the badge number is invalid.
         else {
         document.getElementById("loginStatus").innerHTML = "Badge number is invalid!";
     }
